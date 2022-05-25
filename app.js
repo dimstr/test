c20167ab63fb1cfad115237cce354d00fe0bfa98
dimstr <93518175+dimstr@users.noreply.github.com>
2022-05-25 18:40:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var imgRef_02 = storageRef.child('data/photo_02.jpg');
 
 
 firebase.auth().signInAnonymously().then(function () {
-  imgRef_01.getDownloadURL().then(function (url) {
-    document.querySelector('img_01').src = url;
+  imgRef_01.getDownloadURL().then(function (url_01) {
+    document.querySelector('img_01').src = url_01;
   }).catch(function (error) {
     console.error(error);
   });
-  imgRef_02.getDownloadURL().then(function (url) {
-    document.querySelector('img_02').src = url;
+  imgRef_02.getDownloadURL().then(function (url_02) {
+    document.querySelector('img_02').src = url_02;
   }).catch(function (error) {
     console.error(error);
   });
